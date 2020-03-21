b5abdf9d117d0569b823b7b5ad191916116c47ec
Zoltan Kassai <zoltan.kassai@mirumagency.com>
2020-03-21 10:44:26 +0100

    object based dinamic component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-      <div className='App'>
-          <h1>React</h1>
-          <p>Works!</p>
-          <Person name='Zoli' age='47'/>
-          <Person name='Dommi' age='42'>My Hobbies Racing </Person>
-          <Person name='Atti' age='12'/>
-      </div>
-    );
+class App extends Component {
+    state = {
+        persons: [
+            {name:'argsgs', age:'47'},
+            {name:'xzdgfsv', age:'42'},
+            {name:'zvddfv', age:'12'}
+        ],
+        otherState: 'some other value'
+    }
+
+    switchNameHandler = () => {
+        console.log('Was clicked');
+        this.setState( {
+            persons: [
+                {name:'dagf', age:'47'},
+                {name:'dfgs', age:'42'},
+                {name:'xfvbfv', age:'12'}
+            ]
+        })
+    }
+
+    render() {
+        return (
+          <div className='App'>
+              <h1>React</h1>
+              <p>Works!</p>
+              <button onClick={this.switchNameHandler}>Switch name</button>
+              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies Racing </Person>
+              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          </div>
+        );
+    }
 }
 
 export default App;
