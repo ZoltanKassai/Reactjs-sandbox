e396f6dd69672777eae46df34223dbde8de901b9
Zoltan Kassai <zoltan.kassai@mirumagency.com>
2020-03-23 10:40:55 +0100

    function based dinamic component via state hook

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-    state = {
+const App = props => {
+
+    const [ personsState, setPersonsState ] = useState({
         persons: [
             {name:'argsgs', age:'47'},
             {name:'xzdgfsv', age:'42'},
             {name:'zvddfv', age:'12'}
-        ],
-        otherState: 'some other value'
-    }
+        ]
+    });
 
-    switchNameHandler = () => {
-        console.log('Was clicked');
-        this.setState( {
+    const [otherState, setOtherState] = useState('other value');
+
+    console.log(personsState, otherState);
+
+    const switchNameHandler = () => {
+        setPersonsState( {
             persons: [
-                {name:'dagf', age:'47'},
-                {name:'dfgs', age:'42'},
-                {name:'xfvbfv', age:'12'}
-            ]
-        })
-    }
+                {name:'dagf', age:'147'},
+                {name:'dfgs', age:'142'},
+                {name:'xfvbfv', age:'112'}
+            ],
+            otherState: personsState.otherState
+        });
+    };
 
-    render() {
-        return (
-          <div className='App'>
-              <h1>React</h1>
-              <p>Works!</p>
-              <button onClick={this.switchNameHandler}>Switch name</button>
-              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies Racing </Person>
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-          </div>
-        );
-    }
+    return (
+      <div className='App'>
+          <h1>React</h1>
+          <p>Works!</p>
+          <button onClick={switchNameHandler}>Switch name</button>
+          <Person
+              name={personsState.persons[0].name}
+              age={personsState.persons[0].age}
+          />
+          <Person
+              name={personsState.persons[1].name}
+              age={personsState.persons[1].age}
+          >
+              My Hobbies Racing
+          </Person>
+          <Person
+              name={personsState.persons[2].name}
+              age={personsState.persons[2].age}
+          />
+      </div>
+    );
 }
 
 export default App;
